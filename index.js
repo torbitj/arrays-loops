629ff2a9671ce7ed0b5995bc38620971461ad272
JT <jstorbit@gmail.com>
2025-11-06 17:52:49 -0700

    Add logic to add and remove from beginning and end

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const zoo = ["Panda", "Lion", "Elk"];
 
+zoo.push("bunny", "flamingo");
+
+console.log(zoo);
+
+zoo.unshift("Alligator", "Elephant");
+
+console.log(zoo);
+
+zoo.pop();
+
+console.log(zoo);
+
+zoo.shift();
+
+console.log(zoo);
